fd7f6fc6908f73d986dfc1d6564864f22757c971
Doga Armangil <armangild@yahoo.com>
2012-07-19 10:56:57 +0200

    init

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Linq;
+
+//mvc3 
+namespace HelloWorld.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        public XDocument CallService() {
+            return new XDocument(
+                new XElement(NS+"goto", 
+                    new XAttribute("href", "/Home/ReceiveCallResult"),
+                    new XElement(NS+"call", 
+                        new XAttribute("href", "/Service/Index"),
+                        new XElement("name", new XText("Dave"))
+                    )
+                )
+            );
+        }
+
+        [HttpPost]
+        public ActionResult ReceiveCallResult() {
+            XmlDocument doc = ParseXMLPost(Request);
+            ViewBag.Name = (string)EvaluateXPath(doc, "/*/text()");
+            return View();
+        }
+
+        protected XNamespace NS = "http://qworum.net/";
+
+        protected XmlDocument ParseXMLPost(HttpRequestBase request) {
+            XmlDocument doc = new XmlDocument();
+            using (var reader = new StreamReader(request.InputStream)) {
+                try {
+                    doc.LoadXml(reader.ReadToEnd());
+                } catch (XmlException ex) {
+                    doc = null;
+                }
+            }
+            if (doc == null) {
+                try {
+                    doc = new XmlDocument();
+                    doc.LoadXml(request.QueryString.Get("qworum"));
+                } catch (Exception ex) {
+                    doc = null;
+                }
+            }
+            return doc;
+        }
+
+        protected object EvaluateXPath(XmlDocument doc, string xpath) {
+            try {
+                XPathDocument xpathDoc = new XPathDocument(new XmlNodeReader(doc));
+                XPathNavigator nav = xpathDoc.CreateNavigator();
+                XPathExpression expr = XPathExpression.Compile(xpath);
+                return nav.Evaluate(expr);
+            }catch(Exception ex){}
+            return null;
+        }
+    }
+}
---
+++ HelloWorld/Controllers/ServiceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Xml;
+using System.Xml.Linq;
+
+namespace HelloWorld.Controllers {
+    public class ServiceController : HomeController {
+        new public XDocument Index() {
+            string name = (string)EvaluateXPath(ParseXMLPost(Request), "/*/text()");
+            return new XDocument(
+                new XElement(NS+"goto", new XAttribute("href", "/Service/Show"),
+                    new XElement(NS+"variable", new XAttribute("name", "result"),
+                        new XElement("sentence", new XText(name))
+                    )
+                )
+            );
+        }
+
+        public ActionResult Show() {
+            XmlDocument doc = ParseXMLPost(Request);
+            ViewBag.Sentence = (string)EvaluateXPath(doc, "/*/text()");
+            return View();
+        }
+
+        public XDocument Result() {
+            return new XDocument(
+                new XElement(NS+"variable", new XAttribute("name", "result"))
+            );
+        }
+    }
+}
