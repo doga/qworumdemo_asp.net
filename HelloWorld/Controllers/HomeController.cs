6eedb68e7753b7cf5481e7c678cfb84b45ab1314
Doga Armangil <armangild@yahoo.com>
2012-07-19 18:45:16 +0200

    fix post parsing bug

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Xml;
 using System.Xml.XPath;
 using System.Xml.Linq;
+using System.Web.Helpers;
 
 //mvc3 
 namespace HelloWorld.Controllers {
@@ -22,10 +23,10 @@ namespace HelloWorld.Controllers {
                 new XComment("\n"+
                   "  QWORUM SUPPORT IS MISSING IN YOUR BROWSER:\n"+
                   "  This site only works correctly with Qworum enabled browsers. \n"+
-                  "  Please visit  http://www.qworum.com/products\n"
+                  "  Please visit http://www.qworum.com/products\n"
                 ),
                 new XElement(NS+"goto", 
-                    new XAttribute("href", "/Home/ReceiveCallResult"),
+                    new XAttribute("href", "ReceiveCallResult"),
                     new XElement(NS+"call", 
                         new XAttribute("href", "/Service/Index"),
                         new XElement("name", new XText("Dave"))
@@ -35,29 +36,35 @@ namespace HelloWorld.Controllers {
         }
 
         [HttpPost]
+        [ValidateInput(false)]
         public ActionResult ReceiveCallResult() {
             XmlDocument doc = ParseXMLPost(Request);
-            ViewBag.Sentence = (string)EvaluateXPath(doc, "/*/text()");
+            ViewBag.Sentence = (string)EvaluateXPath(doc, "string(/*/text())");
             return View();
         }
 
-        // boilerplate
+        // Boilerplate
 
         protected XNamespace NS = "http://qworum.net/";
 
         protected XmlDocument ParseXMLPost(HttpRequestBase request) {
-            XmlDocument doc = new XmlDocument();
-            if(request.ContentType.IndexOf("application/xml") >= 0){
-              using (var reader = new StreamReader(request.InputStream)) {
-                  try {
-                      doc.LoadXml(reader.ReadToEnd());
-                  } catch (XmlException ex) {
-                      doc = null;
-                  }
-              }
-            }else{
+            XmlDocument doc = null;
+            if (request.ContentType.IndexOf("application/xml") == 0) {
+                using (var reader = new StreamReader(request.InputStream)) {
+                    try {
+                        doc = new XmlDocument();
+                        string xml = reader.ReadToEnd();
+                        doc.LoadXml(xml);
+                    } catch (XmlException ex) {
+                        doc = null;
+                    }
+                }
+            } else if (request.ContentType.IndexOf("application/x-www-form-urlencoded") == 0) {
                 try {
-                    doc.LoadXml(request.QueryString.Get("qworum"));
+                    doc = new XmlDocument();
+                    FormCollection form = new FormCollection(Request.Unvalidated().Form);
+                    string xml = form.Get("qworum");
+                    doc.LoadXml(xml);
                 } catch (Exception ex) {
                     doc = null;
                 }
--- HelloWorld/Controllers/ServiceController.cs
+++ HelloWorld/Controllers/ServiceController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 using System.Xml;
 using System.Xml.Linq;
+using System.Web.Helpers;
 
 namespace HelloWorld.Controllers {
     public class ServiceController : HomeController {
@@ -14,16 +15,17 @@ namespace HelloWorld.Controllers {
          * Receive call argument, compute result
          */
         [HttpPost]
+        [ValidateInput(false)]
         new public XDocument Index() {
-            string name = "none";
-            try{
-              name=(string)EvaluateXPath(ParseXMLPost(Request), "/*/text()");
-            }catch(Exception ex){}
+            // input
+            string name = (string)EvaluateXPath(ParseXMLPost(Request), "string(/*/text())");
+            // output
+            string sentence = "Hello, "+name;
             Response.ContentType="application/xml";
             return new XDocument(
-                new XElement(NS+"goto", new XAttribute("href", "/Service/Show"),
+                new XElement(NS+"goto", new XAttribute("href", "Show"),
                     new XElement(NS+"variable", new XAttribute("name", "result"),
-                        new XElement("sentence", new XText("Hello, "+name))
+                        new XElement("sentence", new XText(sentence))
                     )
                 )
             );
@@ -34,7 +36,7 @@ namespace HelloWorld.Controllers {
          */
         public ActionResult Show() {
             XmlDocument doc = ParseXMLPost(Request);
-            ViewBag.Sentence = (string)EvaluateXPath(doc, "/*/text()");
+            ViewBag.Sentence = (string)EvaluateXPath(doc, "string(/*/text())");
             return View();
         }
 
